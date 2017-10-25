334aefbf246ceb9607c39ff85fcfade29bc5abaa
Adam Smith and Mikey Pinaud <adam@>
2017-10-25 09:10:44 -0700

    add a function to evaluate user input and return whether or not user year is a leap

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+var leapYear = function(year) {
+  if ((year % 100 !== 0) && (year % 4 === 0) || (year % 400 === 0)) {
+    return true;
+  } else {
+    return false;
+  };
+};
+
+
+$(function(){
+ $("#leap-year").submit(function(event) {
+   event.preventDefault();
+   var year = parseInt($("#year").val());
+   var result = leapYear(year);
+   $("#result").text(result);
+ });
+});
